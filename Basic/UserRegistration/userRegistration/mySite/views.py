5751e798b3b0cc829a93646bcbec3c7bb410f59d
JEunLe <hemslje@naver.com>
2020-01-20 17:29:14 +0900

    commit
    email, logout complete

--- Basic/UserRegistration/userRegistration/mySite/views.py
+++ Basic/UserRegistration/userRegistration/mySite/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.contrib.auth import logout, get_user_model
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
